b612efdc9eef18ca730aa2bc4ea51b134c6ab5fc
jun_lin <junlin5525@gmail.com>
2023-04-04 17:01:34 +0800

    新增自製的手機販售頁面

--- P05_Model_DB/mysite/admin.py
+++ P05_Model_DB/mysite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from mysite import models as mysite_models
 # Register your models here.
+
+admin.site.register(mysite_models.Maker)
+admin.site.register(mysite_models.PModel)
+admin.site.register(mysite_models.Product)
+admin.site.register(mysite_models.PPhoto)
